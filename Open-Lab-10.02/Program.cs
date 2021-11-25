6a24f0d5c557f815981fcc1f7a6eb044c823bda4
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-11-25 10:42:15 +0100

    I defined Book ´s atributions

--- Open-Lab-10.02/Program.cs
+++ Open-Lab-10.02/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace Open_Lab_10._02
 {
     class Program
@@ -7,13 +6,34 @@ namespace Open_Lab_10._02
         static void Main(string[] args)
         {
             Book LOTR = new Book();
-            LOTR.title = "Peter Marcin";
-            Console.WriteLine(LOTR.title);
+            LOTR.Title("Petrov Život");
+            LOTR.Pages(69);
+            LOTR.Cathegory("Funny");
+            LOTR.Author("Ján Kábel");
+            LOTR.RelaseDate(2020);
+            LOTR.Write();
         }
         class Book
         {
-            public string title;
+            private string title;
             private int pages;
+            private string cathegory;
+            private string author;
+            private int relaseDate;
+            public void Title(string newTitle) { title = newTitle; }
+            public void Pages(int newPages) { pages = newPages; }
+            public void Cathegory(string newCathegory) { cathegory = newCathegory; }
+            public void Author(string newAuthor) { author = newAuthor; }
+            public void RelaseDate(int newRelaseDate) { relaseDate = newRelaseDate; }
+            public void Write()
+            {
+                Console.WriteLine("Názov: " + title);
+                Console.WriteLine("Počet strán: " + pages);
+                Console.WriteLine("Žáner: " + cathegory);
+                Console.WriteLine("Autor: " + author);
+                Console.WriteLine("Dátum vydania: " + relaseDate);
+                
+            }
         }
     }
 }
