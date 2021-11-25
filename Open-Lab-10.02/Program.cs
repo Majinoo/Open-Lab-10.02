f2cb542f64f2f448ab6e00301266990f7db3f296
Majinoo <91146809+Majinoo@users.noreply.github.com>
2021-11-25 09:22:47 +0100

    Add project files.

---
+++ Open-Lab-10.02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Open_Lab_10._02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Book LOTR = new Book();
+            LOTR.title = "Peter Marcin";
+            Console.WriteLine(LOTR.title);
+        }
+        class Book
+        {
+            public string title;
+            private int pages;
+        }
+    }
+}
